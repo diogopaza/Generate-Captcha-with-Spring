1d02f296024936de84b5a8968c984a0a47c13431
Diogo Paza <diogopazacvel@gmail.com>
2022-10-01 16:46:56 -0300

    start the project

---
+++ captcha/src/test/java/com/diogo/captcha/captcha/CaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogo.captcha.captcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
